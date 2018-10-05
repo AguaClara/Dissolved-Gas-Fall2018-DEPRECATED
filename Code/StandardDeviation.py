7d47e7a1c91f1b9f6869ad3f38da97d5d6c1b09c
Thomas Bradford <35972352+Thomas-Bradford@users.noreply.github.com>
2018-10-05 16:45:32 -0400

    Add files via upload

---
+++ Code/StandardDeviation.py
@@ -0,0 +1,26 @@
+# Python code for standard deviation
+
+import math
+
+values = []
+item = "blank"
+for n in range(10):
+    item = input("What item would you like to add to the list? If the list is complete, type 'quit' ")
+    if item == "quit":
+        break
+    else:
+        values.append(float(item))
+
+total=0
+
+for n in range(len(values)):
+    total += values[n]
+
+average= total / len(values)
+
+numerator = 0
+for n in range(len(values)):
+    numerator += (values[n]-average)**2
+
+standard_deviation = math.sqrt(numerator / (len(values)-1))
+print("The standard deviation in the given list is " + str(standard_deviation))

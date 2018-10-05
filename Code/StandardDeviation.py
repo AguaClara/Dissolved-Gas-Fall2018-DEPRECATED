9d0bc4af311fd3d3129b445452772881276ef64b
Thomas Bradford <tsb49@cornell.edu>
2018-10-05 16:46:53 -0400

    Edited assumption

--- Code/StandardDeviation.py
+++ Code/StandardDeviation.py
@@ -1,10 +1,11 @@
-# Python code for standard deviation
+# Python code for standard deviation of a list of length 100 or less
+# Assumption: All entries are real numbers
 
 import math
 
 values = []
 item = "blank"
-for n in range(10):
+for n in range(100):
     item = input("What item would you like to add to the list? If the list is complete, type 'quit' ")
     if item == "quit":
         break

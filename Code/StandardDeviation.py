fed9b6ac33ef59d47fe355bd51e985e0ad7f5cf4
Thomas Bradford <tsb49@cornell.edu>
2018-11-02 17:11:56 -0400

    Improving the code

--- Code/StandardDeviation.py
+++ Code/StandardDeviation.py
@@ -9,11 +9,12 @@ import aide_design as ad
 from aide_design import utility as ut
 from aide_design.play import*
 
+print("\n"+"Please input only real numbers; make sure the list is at least two items long! If you're done inputting your list, enter type 'done'."+"\n")
 values = []
 item = "blank"
 for n in range(100):
-    item = input("What item would you like to add to the list? If the list is complete, type 'quit' ")
-    if item == "quit":
+    item = input("What item would you like to add to the list?" +"\n")
+    if item == "done":
         break
     else:
         try:
@@ -32,5 +33,6 @@ for n in range(len(values)):
     numerator += (values[n]-average)**2
 
 standard_deviation = math.sqrt(numerator / (len(values)-1))
-print("The standard deviation in the given list is " + str(ut.sig(standard_deviation, 3)))
-print("The average value in the given list is " + str(ut.sig(average, 3)))
+
+print("The standard deviation in your list is " + str(ut.sig(standard_deviation, 3))+'.')
+print("The average value in your list is " + str(ut.sig(average, 3))+".")

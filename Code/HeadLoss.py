57188608bee810febcd021c8c5d832b3c86b8c20
Isaac Singer <ibs27@cornell.edu>
2018-11-21 15:24:42 -0500

    Head Loss Code (TODO: FIX UNITS)

---
+++ Code/HeadLoss.py
@@ -0,0 +1,17 @@
+import math
+import numpy as nm
+import aide_design as ad
+from aide_design import utility as ut
+from aide_design.play import*
+
+viscosity = float(input("What is the kinematic viscosity of water, in units of millimeters squared per second?\n"))*u.mm**2/u.s
+effluent_tubing_length = float(input("What is the length of the effluent tubing, in units of centimeters?\n"))*u.cm
+velocity = float(input("What is the velocity of water in the effluent piping, in units of millimeters per second?\n"))*u.mm/u.s
+
+density_water = 997*u.kg/(u.m**3)
+g = 9.8 *u.m/(u.s**2)
+pipe_diameter = float(input("What is the diameter of the effluent piping, in units of millimeters?\n"))*u.mm
+
+Head_Loss = (32 * viscosity * effluent_tubing_length * velocity)/(density_water * g * pipe_diameter**2)
+
+print("The Head Loss due to the effluent tubing is "+ str(ut.sig(Head_Loss,3))+".")
